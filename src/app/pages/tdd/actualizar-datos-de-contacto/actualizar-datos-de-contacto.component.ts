be8d00cef91e3eb39e17e2e24bd1aa496fd31bf3
David <dcruz@ariconsultoria.com>
2018-03-14 12:30:22 -0600

    =Se agrego el flujo de pantallas de activar alertas, y el flujo de pantallas actualizar datos de contacto sin agregar en el routes de pages tdd

---
+++ src/app/pages/tdd/actualizar-datos-de-contacto/actualizar-datos-de-contacto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-datos-de-contacto',
+  templateUrl: './actualizar-datos-de-contacto.component.html',
+})
+export class ActualizarDatosDeContactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
