b8231c86badb1e6fc8f7d08752cbcc3604ab4b8c
RicardoFloresMtz <rflores@ariconsultoria.com>
2018-03-15 13:21:50 -0600

    Implementacion teclado, anexo a esto deben ejecutar los comandos npm i @angular/material, @angular/cdk , --save @nhx-material-keyboard/core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,46 @@ import { AppComponent } from './app.component';
 
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { APP_ROUTES } from './app.routes';
 import { LoginComponent } from './login/login.component';
 import { PagesModule } from './pages/pages.module';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { IKeyboardLayouts, keyboardLayouts, MAT_KEYBOARD_LAYOUTS, MatKeyboardModule, KeyboardClassKey } from '@ngx-material-keyboard/core';
+
+const customLayouts: IKeyboardLayouts = {
+  ...keyboardLayouts,
+  'Numerico': {
+    'name': 'NumPad',
+    'keys': [
+      [
+        ['1', '!', '|'],
+        ['2', '"', '@'],
+        ['3', '\'', '#'],
+        ['4', '$', '~']
+      ],
+      [
+        ['5', '%', '\u20ac'],
+        ['6', '&', '\u00ac'],
+        ['7', '/'],
+        ['8', '('],
+      ],
+      [
+        ['9', ')'],
+        ['0', '='],
+        ['-', ';'],
+        ['.', ':']
+      ],
+      [
+        [KeyboardClassKey.Space, KeyboardClassKey.Space, KeyboardClassKey.Space, KeyboardClassKey.Space],
+      ]
+    ],
+    'lang': ['num']
+  }
+};
 
 @NgModule({
   declarations: [
@@ -25,8 +59,12 @@ import { HttpModule } from '@angular/http';
     PagesModule,
     HttpModule,
     AngularFontAwesomeModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatKeyboardModule,
   ],
-  providers: [],
+  providers: [{ provide: MAT_KEYBOARD_LAYOUTS, useValue: customLayouts}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
