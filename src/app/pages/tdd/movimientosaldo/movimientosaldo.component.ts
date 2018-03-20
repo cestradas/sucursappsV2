5f3b21206e61ea01a509324c68e89bf5e5d03831
David <dcruz@ariconsultoria.com>
2018-03-20 12:47:15 -0600

    Se modificaron los archivos movimientosaldo.html y movimientosaldo.ts para que pinte la respuesta en el formulario de la derecha

--- src/app/pages/tdd/movimientosaldo/movimientosaldo.component.ts
+++ src/app/pages/tdd/movimientosaldo/movimientosaldo.component.ts
@@ -7,27 +7,33 @@ import { Component, OnInit } from '@angular/core';
 export class MovimientosaldoComponent implements OnInit {
 DatosJSON: any;
 ALIAS: any;
+alias: any;
 TamArray: any;
 numPaginas: any;
+tamPaginas: any = 8;
 arrayNumPag:  Array<any> = [];
 NumeroDatoInicial: any = 0;
-NumeroDatoFinal: any = 5;
+NumeroDatoFinal: any = 8;
 Comparador: any;
+contador: any;
 constructor() {
 
 
   }
 
   ngOnInit() {
-    this.movimientos();
+     this.movimientos();
+    this.consultaClabeSaldo ("1", "0100000034");
   }
+
+
 movimientos() {
   // tslint:disable-next-line:max-line-length
   const Infocuentas = '{"Id":"1","ArrayCuentas":[{"Alias":"Suma N\u00f3mina","IdCuenta":92425410,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425411,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425412,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"},{"Alias":"Suma N\u00f3mina","IdCuenta":92425467,"NoCuenta":"0101361424"}],"MensajeAUsuario":"SUCCESS"}';
   this.DatosJSON = JSON.parse(Infocuentas);
   this.ALIAS = this.DatosJSON.ArrayCuentas;
   this.TamArray = this.ALIAS.length;
-  this.numPaginas = this.TamArray / 5;
+  this.numPaginas = this.TamArray / this.tamPaginas;
   let i = 0;
   for (i; i < this.numPaginas; i ++) {
     this.arrayNumPag.push(i);
@@ -37,27 +43,62 @@ movimientos() {
 }
 
 Cambiarpagina(NumeroPag) {
-  this.NumeroDatoFinal = (NumeroPag.textContent ) * 5 ;
-  this.NumeroDatoInicial = this.NumeroDatoFinal - 5;
+  this.NumeroDatoFinal = (NumeroPag.textContent ) * this.tamPaginas ;
+  this.NumeroDatoInicial = this.NumeroDatoFinal - this.tamPaginas;
 }
 
     PreviousPag (NumPrev) {
-      if (NumPrev <= 5) {
+      if (NumPrev <= this.tamPaginas) {
         this.NumeroDatoInicial = 0;
-        this.NumeroDatoFinal = 5;
+        this.NumeroDatoFinal = this.tamPaginas;
       } else {
-        this.NumeroDatoFinal = NumPrev - 5;
-        this.NumeroDatoInicial = this.NumeroDatoFinal - 5;
+        this.NumeroDatoFinal = NumPrev - this.tamPaginas;
+        this.NumeroDatoInicial = this.NumeroDatoFinal - this.tamPaginas;
       }
     }
 
     NextPag (NumNext) {
+
       if (NumNext >= this.TamArray ) {
         this.NumeroDatoFinal = this.TamArray;
-        this.NumeroDatoInicial = this.NumeroDatoFinal - 5;
+        for ( let i = 0; i < this.numPaginas; i++) {
+          this.contador = i;
+        }
+        this.NumeroDatoInicial = (this.contador) * this.tamPaginas;
       } else {
-        this.NumeroDatoFinal = NumNext + 5;
-        this.NumeroDatoInicial = this.NumeroDatoFinal - 5;
+        this.NumeroDatoFinal = NumNext + this.tamPaginas;
+        this.NumeroDatoInicial = this.NumeroDatoFinal - this.tamPaginas;
       }
+    }
+
+
+    
+    consultaClabeSaldo(tipoMovimiento, numeroCuenta) {
+      const this_aux = this;
+      console.log("si entre: ", tipoMovimiento, numeroCuenta);
+      const formParameters = {
+        txtTipoMov : tipoMovimiento, 
+        txtNumCuenta: numeroCuenta
+      }; 
+      const resourceRequest = new WLResourceRequest(
+        'adapters/AdapterBanorteSucursApps/resource/ConsultaClabesSaldos', WLResourceRequest.POST);
+        resourceRequest.setTimeout(30000);
+        
+        resourceRequest.sendFormParameters(formParameters).then(
+          function(response) {
+           console.log(response.responseText);
+           this_aux.alias = response.responseJSON;
+           console.log(this_aux.alias);
+           console.log(this_aux.alias.SaldoDisponible);
+            const detalleCuenta = response.responseJSON;
+            if ( detalleCuenta.Id === '1') {
+
+              const textTitular = detalleCuenta.SaldoDia;
+            } else {
+              console.log(detalleCuenta.MensajeAUsuario);
+            }
+          }, function(error) {
+      });
+
     }
 }
