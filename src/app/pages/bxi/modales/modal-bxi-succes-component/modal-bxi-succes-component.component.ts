a18d0985812222199ca07fdf60f8e680d416a319
MiguelGarcia9 <mgarcia@ariconsultoria.com>
2018-03-21 15:31:40 -0600

    =se agregan componentes de modales de confirmacion y error BXI

---
+++ src/app/pages/bxi/modales/modal-bxi-succes-component/modal-bxi-succes-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-bxi-succes-component',
+  templateUrl: './modal-bxi-succes-component.component.html',
+  styles: []
+})
+export class ModalBxiSuccesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
