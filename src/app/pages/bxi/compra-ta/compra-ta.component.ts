666c54d7f5ce0e793aa40d7030ef27f9ce0994d2
MiguelGarcia9 <mgarcia@ariconsultoria.com>
2018-04-04 15:25:20 -0500

    =modificacion de transferencias spei

--- src/app/pages/bxi/compra-ta/compra-ta.component.ts
+++ src/app/pages/bxi/compra-ta/compra-ta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
@@ -18,20 +18,21 @@ declare var $: $;
 })
 export class CompraTaComponent implements OnInit {
 
-  postResp;
-  tipoMovimiento: string;
-  numeroCuenta: string;
+  @ViewChild('listaCuentas', { read: ElementRef}) listaCuentas: ElementRef ;
+ 
+
+  listaCuentasBen: Array<any> = [];
+  
   datosCuenta: any[] = [];
-  box_price = 0;
-  box_price_formatted = "$10.00";
 
   constructor( private _http: Http, private router: Router, public service: SesionBxiService, private renderer: Renderer2 ) { }
 
   ngOnInit() {
      this.consultaCatEmpresas();
+     this.fillSelectCuentas();
   }
 
-
+  
   consultaCatEmpresas() {
     const this_aux =  this;
     const operacionesbxi: OperacionesBXI = new OperacionesBXI();
@@ -89,6 +90,63 @@ export class CompraTaComponent implements OnInit {
         });
   }
 
+  fillSelectCuentas() {
+    const this_aux = this;
+    const cuentasString = this_aux.service.infoCuentas;
+    console.log(this_aux.service.infoCuentas);
+    const consultaCuentas = JSON.parse(cuentasString);
+    const cuentasArray = consultaCuentas.ArrayCuentas;
+      cuentasArray.forEach(cuenta => {
+          const li =  this.renderer.createElement('li');
+          const a = this.renderer.createElement('a');
+          const textoCuenta = this.renderer.createText( cuenta.Alias);
+          this.renderer.setProperty(a, 'value', cuenta.NoCuenta);
+          this. renderer.listen(a, 'click', (event) => { this_aux.setDatosCuentaSeleccionada(event.target); });
+          this.renderer.appendChild(a, textoCuenta),
+          this.renderer.appendChild(li, a);
+          this.renderer.appendChild(this.listaCuentas.nativeElement, li);
+    });
+
+}
+
+setDatosCuentaSeleccionada(elementHTML) {
+
+  const this_aux = this;
+  console.log(elementHTML);
+  const tableOrigen = document.getElementById('tableOrigen');
+  const tableDefaultOrigen = document.getElementById('tableDefaultOrigen');
+  const lblCuentaOrigen = document.getElementById('lblCuentaOrigen');
+  const lblAliasOrigen = document.getElementById('lblAliasOrigen');
+  const numCuenta_seleccionada = elementHTML.value;
+
+  tableOrigen.setAttribute('style', 'display: block');
+  tableDefaultOrigen.setAttribute('style', 'display: none');
+
+  lblAliasOrigen.innerHTML = elementHTML.textContent;
+  lblCuentaOrigen.innerHTML = numCuenta_seleccionada.toString();
+  this_aux.service.numCuentaSeleccionado = numCuenta_seleccionada;
+  this_aux.getSaldoDeCuenta(numCuenta_seleccionada);
+
+  
+}
+
+getSaldoDeCuenta(numCuenta_seleccionada) {
+ 
+  const operacionesbxi: OperacionesBXI = new OperacionesBXI();
+  operacionesbxi.getSaldo(numCuenta_seleccionada).then(
+      function(response1) {
+        console.log(response1.responseText);
+        const detalleSaldos = response1.responseJSON;
+        if ( detalleSaldos.Id === '1') {
+          const lblSaldoOrigen = document.getElementById('lblSaldoOrigen');
+          lblSaldoOrigen.innerHTML = detalleSaldos.SaldoDisponible;
+        } else {
+          console.log(detalleSaldos.MensajeAUsuario);
+        }
+      }, function(error) {
+  });
+}
+
 
   insertaSaldo(id) {
 
@@ -99,7 +157,7 @@ export class CompraTaComponent implements OnInit {
     
     console.log(id.id);
 
-    operacionesbxi.getSaldo(id.id).then(
+    operacionesbxi.getSaldoCompany(id.id).then(
       function(response1) {
         console.log(response1.responseText);
         const detalleSaldos = response1.responseJSON;
