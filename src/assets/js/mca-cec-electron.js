92f680970cb7ee564b1f356dbf1042bcf48368e0
MiguelGarcia9 <mgarcia@ariconsultoria.com>
2018-06-11 17:01:33 -0500

    =EDC mca-cec-electron

--- src/assets/js/mca-cec-electron.js
+++ src/assets/js/mca-cec-electron.js
@@ -14,8 +14,7 @@ function saveDocElectron(documentoB64, nombremDoc) {
     console.log(fs1);
     console.log(documentoB64);
     console.log(nombremDoc);
-    //console.log(numDoc);
-    //console.log(fechaDoc);
+
     // electron para DOCUMENTO
 
     // var tmpFileName0 = 'c:/temp/electron/' + 'D_' + numDoc + '_' + fechaDoc + '.pdf';
@@ -37,6 +36,7 @@ function saveDocElectron(documentoB64, nombremDoc) {
         console.log("borrado de doc");
     }, 2000);
 
+    //Llama a servicio de impresion de docuemnto
     callPrinter(nombremDoc);
 }
 
@@ -45,7 +45,26 @@ function callPinPad() {
 
     var url = 'http://localhost:8081/sucursappsdevices/pinpad/read';
 
-    fetch(url).then((resp) => { return resp.text() }).then((text) => { console.log(text) });
+    // fetch(url).then((resp) => { return resp.text() }).then((text) => { console.log(text) });
+
+    fetch(url).then(function(response) {
+        // Convert to JSON
+        return response.json();
+    }).then(function(res) {
+
+        console.log(res);
+
+        localStorage.setItem("tr2", res.tr2);
+        localStorage.setItem("np", res.np);
+        localStorage.setItem("res", res.res);
+
+
+    }, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+    });
 
 }
 
@@ -53,67 +72,7 @@ function callPrinter(nombremDoc) {
 
     var url = 'http://localhost:8082/sucursappsdevices/printer/pdf?f=' + nombremDoc + '.pdf';
 
-    fetch(url).then((resp) => { return resp.text() }).then((text) => { console.log(text) });
-
-
-
-}
-
-
-
-
-
-
-//console.log("Escribiendo archivo: " + tmpFileName);
-//var numeroDocs = arrPDFS.length;
-
-
-function getPDFS(arrFechasPDFS, PDFS) {
-    console.log("getPDFS");
-    console.log("getPDFS fechas: " + arrFechasPDFS);
-
-
-    //	for(var i=0; i<=arrPDFS.length; i++){
-    var fs1 = require("fs");
-    var fs2 = require("fs");
-    var fs3 = require("fs");
-
-    if (arrPDFS[0] != null) {
-        var tmpFileName0 = 'c:/temp/' + 'D_1_' + numeroCuenta + '_' + fechasEnvioMail[0] + '.pdf';
-        //		setTimeout(function() {
-        fs1.writeFile(tmpFileName0, arrPDFS[0], 'base64', function(err) {
-            if (err) {
-                return console.log(err);
-            }
-
-        });
-    }
-
-    if (arrPDFS[1] != null) {
-        //		setTimeout(function(){	
-        var tmpFileName1 = 'c:/temp/' + 'D_2_' + numeroCuenta + '_' + fechasEnvioMail[1] + '.pdf';
-        fs2.writeFile(tmpFileName1, arrPDFS[1], 'base64', function(err) {
-            if (err) {
-                return console.log(err);
-            }
-
-        });
-        //		}, 10000);
-    }
-
-    if (arrPDFS[2] != null) {
-        //		setTimeout2(function(){	
-        var tmpFileName2 = 'c:/temp/' + 'D_3_' + numeroCuenta + '_' + fechasEnvioMail[2] + '.pdf';
-        fs3.writeFile(tmpFileName2, arrPDFS[2], 'base64', function(err) {
-            if (err) {
-                return console.log(err);
-            }
-
-        });
-        //			}, 20000);
-    }
-    //	}
-
+    fetch(url).then((resp) => { return resp.text() }).then((text) => { console.log("Estatus impresion:" + text) });
 
 
 
