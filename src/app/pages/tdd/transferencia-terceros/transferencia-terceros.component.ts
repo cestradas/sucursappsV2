aeb56871e795a3b6f0ff4f44a0bd251b90359b3b
MiguelGarcia9 <mgarcia@ariconsultoria.com>
2018-06-25 10:43:24 -0500

    =Transferencias terceros TDD VALIDA NIP

--- src/app/pages/tdd/transferencia-terceros/transferencia-terceros.component.ts
+++ src/app/pages/tdd/transferencia-terceros/transferencia-terceros.component.ts
@@ -144,14 +144,18 @@ export class TransferenciaTercerosComponent implements OnInit {
         if (res === true) {
 
           $('#ModalTDDLogin').modal('hide');
+          setTimeout( () => $('#_modal_please_wait').modal('hide'), 500 );
           this.transTerceroTransaccion();
+          this._validaNipService.respuestaNip.res = "";
 
         } else {
 
           console.error("Mostrar modal las tarjetas no son iguales");
-          document.getElementById('mnsError').innerHTML =   "Las tarjetas no corresponden.";
+          document.getElementById('mnsError').innerHTML =   "El NIP introducido no corresponde.";
           $('#_modal_please_wait').modal('hide');
           $('#errorModal').modal('show');
+          $('#ModalTDDLogin').modal('hide');
+          this._validaNipService.respuestaNip.res = "";
 
 
         }
