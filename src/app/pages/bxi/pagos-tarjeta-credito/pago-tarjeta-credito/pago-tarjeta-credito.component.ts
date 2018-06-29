e39dc79af8860bb4f0ebb5ea58430dc68f02e362
RicardoFloresMtz <rflores@ariconsultoria.com>
2018-06-29 14:03:39 -0500

    Correccion mascara de dinero 3

--- src/app/pages/bxi/pagos-tarjeta-credito/pago-tarjeta-credito/pago-tarjeta-credito.component.ts
+++ src/app/pages/bxi/pagos-tarjeta-credito/pago-tarjeta-credito/pago-tarjeta-credito.component.ts
@@ -278,7 +278,7 @@ export class PagoTarjetaCreditoComponent implements OnInit {
     const operacionesbxi: OperacionesBXI = new OperacionesBXI();
     this_aux.CuentaOrigen = operacionesbxi.mascaraNumeroCuenta( this_aux.service.numCuentaSeleccionado);
     this_aux.nombreBanco = this_aux.service.nameBancoDestino;
-    this_aux.ImporteShow = parseInt(montoAPagar, 10);
+    this_aux.ImporteShow = parseInt(this_aux.replaceSimbolo( montoAPagar), 10);
     this_aux.Importe = this_aux.replaceSimbolo( montoAPagar);
     this_aux.validarSaldo(this_aux.service.numCuentaSeleccionado, this_aux.Importe);
 
